7e7b97a79ff76af9a76c949c924dec3a0f5ddd8e
Wisp Zei <wzei@il-k5.msk.21-school.ru>
2020-12-04 15:29:29 +0300

    draft of step2

--- wzei/step2_eval.py
+++ wzei/step2_eval.py
@@ -1,73 +1,82 @@
 import sys
+import types
 
-class REPL:
-    def __init__(self):
-        pass
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
 
-    def repl_read(self, line: str) -> str:
-        return line
-    
-    def repl_eval(self, ast: list, env: dict) -> str:
-        return ast
-    
-    def repl_print(self, exp: list) -> str:
-        return self.pr_str(exp)
-    
-    def repl_rep(self, line: str) -> str:
-        ret = self.repl_read(line)
-        ast = self.parse(self.read_str(ret))
-        return self.repl_print(self.repl_eval(ast, {}))
+def repl_read(line: str) -> str:
+    return line
 
-    def read_str(self, s: str) -> list:
-        "Convert a string into a list of tokens."
-        return s.replace('(',' ( ').replace(')',' ) ').split()
+def repl_eval(ast: list, env: dict) -> int:
+    arg = []
+    for i in ast:
+        if type(i) is list:
+            arg.append(repl_eval(i, env))
+        elif i in env:
+            arg.append(env[i])
+        elif RepresentsInt(i):
+            arg.append(int(i))
+    return arg[0](arg[1], arg[2])
 
-    def parse(self, tokens: list) -> list:
-        token = tokens.pop(0)
-        if '(' == token:
-            L = []
-            while tokens[0] != ')':
-                L.append(self.parse(tokens))
-            tokens.pop(0) # pop off ')'
-            return L
-        else:
-            return str(token)
+def repl_print(exp: list) -> str:
+    return pr_str(exp)
 
+def repl_rep(line: str) -> str:
+    ret = repl_read(line)
+    ast = parse(read_str(ret))
+    return repl_print(repl_eval(ast, repl_env))
 
-    def pr_str(self, ast: list) -> str:
-        "Convert a Python object back into a Lisp-readable string."
-        if isinstance(ast, list):
-            return '(' + ' '.join(map(self.pr_str, ast)) + ')'
-        else:
-            return str(ast)
-        
+def read_str(s: str) -> list:
+    "Convert a string into a list of tokens."
+    return s.replace('(',' ( ').replace(')',' ) ').split()
 
-
-#---------------------------------------
-
-class terminal(object):
-    def __init__(self, func):
-        self.func = func
-    
-    def __call__(self) -> str:
-        print("user> ", end="")
-        return self.func()
+def parse(tokens: list) -> list:
+    token = tokens.pop(0)
+    if '(' == token:
+        L = []
+        while tokens[0] != ')':
+            L.append(parse(tokens))
+        tokens.pop(0) # pop off ')'
+        return L
+    else:
+        return str(token)
 
 
-@terminal
+def pr_str(ast: list) -> str:
+    "Convert a Python object back into a Lisp-readable string."
+    if isinstance(ast, list):
+        return '(' + ' '.join(map(pr_str, ast)) + ')'
+    else:
+        return str(ast)
+
 def my_readline() -> str:
-	return input()
+    print("user> ", end="")
+    return input()
 #---------------------------------------
+
+repl_env = {}
+repl_env['+'] = lambda a,b: a+b
+repl_env['-'] = lambda a,b: a-b
+repl_env['*'] = lambda a,b: a*b
+repl_env['/'] = lambda a,b: int(a/b)
 
 # repl loop
-while True:
-    try:
-        repl = REPL()
-        line = my_readline()
-        if line == None: break
-        if line == "": continue
-        print(repl.repl_rep(line))
-    except EOFError:
-        exit()
-    except Exception as e:
-        print("".join(traceback.format_exception(*sys.exc_info())))
+def repl():
+    while True:
+        try:
+            line = my_readline()
+            if line == None: break
+            if line == "": continue
+            print(repl_rep(line))
+        except EOFError:
+            exit()
+        except Exception as e:
+            print("".join(traceback.format_exception(*sys.exc_info())))
+
+
+if __name__ == '__main__':
+    repl()
